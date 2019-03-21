3402a486222680b4a31a2f246e19109e18710c56
JacobABrennan <jacobabrennan@gmail.com>
2019-03-21 18:40:43 -0400

    Finish ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,7 +16,31 @@ char* msg3 = "hello world #3";
 
 int main(void)
 {
-    // Your code here
-    
+    // Initialize buffer and pipe
+    char reception_buffer[MSGSIZE];
+    int com_pipe[2];
+    // Open pipe
+    int pipe_result = pipe(com_pipe);
+    // Handle errors opening pipe
+    if(pipe_result < 0)
+    {
+        fprintf(stderr, "Failed to open pipe.\n");
+        exit(1);
+    }
+    // Start child (writing) process
+    if(!fork())
+    {
+        write(com_pipe[1], msg1, MSGSIZE);
+        write(com_pipe[1], msg2, MSGSIZE);
+        write(com_pipe[1], msg3, MSGSIZE);
+        return 0;
+    }
+    // Read data (parent process)
+    for(int message_index=0; message_index<3; message_index++)
+    {
+        read(com_pipe[0], reception_buffer, MSGSIZE);
+        printf("Message Received: %s\n", reception_buffer);
+    }
+    // End process
     return 0;
 }
